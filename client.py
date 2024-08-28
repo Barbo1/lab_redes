6c0e352eeab4fb6fc0e6cbde03de5c9a465a16eb
mariana.cabral <mariana.cabral@fing.edu.uy>
2024-08-27 23:46:39 -0300

    cliente

--- client.py
+++ client.py
@@ -1,2 +1,17 @@
 
-class Client():
+class Client(object):
+    def __init__(self, address, host):
+        # pedirle al servidor los metodos que tiene
+        # añadir los metodos al objeto conection resultante
+        # 
+        return self
+    
+    def __call__(self, method, param):
+        print(param)
+    
+def connect(address: str, port: int) -> Client :
+    return Client(address, port)
+
+conn = connect("127.0.0.1", 32)
+conn.proc1(1)
+
