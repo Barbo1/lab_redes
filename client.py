e0c9c47d097525ff1d8bedd61308af3fe707b146
FJBS <Joaquin12201@hotmail.com>
2024-08-28 01:03:51 -0300

    error cuando se ejecuta mas de una operacion

--- client.py
+++ client.py
@@ -1,37 +1,63 @@
 from socket import *
 import json
+import random
+
+def generador_de_ids():
+    return random.randint(0, 10000000000000)
 
 class Client(object):
     sock = None
-    port = 80
+    jsonrpc_version = "2.0"
+    buffer_receptor = 1024
 
+    # Clase auxlilar para realizar el llamado al servidor
     class Client_call_ret(object):
-        sock = None
+        client_father = None
+        method = None
 
-        def __init__(self, socket):
-            self.sock = socket
+        def __init__(self, client, method):
+            self.client_father = client
+            self.method = method
 
         def __call__(self, *args):
-            if self.sock != None:
-                self.sock.send("1".encode())
-                data = self.sock.recv(1024)
-                print(data.decode())
-            else:
+            if self.client_father is None:
+                raise Exception("Cliente no encontrado.")
+            elif self.client_father.sock is None:
                 raise Exception("Socket no encontrado.")
+            elif self.method == "":
+                raise Exception("No hay nomber para el metodo.")
+            else:
+                data_a_enviar = {
+                        "jsonrpc": self.client_father.jsonrpc_version,
+                        "method": self.method,
+                        "params": list(args),
+                        "id": generador_de_ids()
+                    }
+                data_encoded = json.dumps(data_a_enviar).encode()
+                self.client_father.sock.send(data_encoded)
+
+                data_recivida = self.client_father.sock.recv(
+                        self.client_father.buffer_receptor
+                    )
+                print(data_recivida)
+                data_procesada = json.loads(data_recivida.decode())
+                return data_procesada["result"]
 
     def __init__(self, address, port):
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.sock.connect((address, port))
-        pass
-    
+
     def __getattr__(self, method):
-        return self.Client_call_ret(self.sock)
+        return self.Client_call_ret(self, method)
 
     def __del__(self):
         self.sock.close()
-        
-def connect(address: str, port: int) -> Client :
+
+def connect(address: str, port: int)-> Client:
     return Client(address, port)
 
-conn = connect("127.0.0.1", 8080)
-a = conn.proc1()
+conn = connect("127.0.0.1", 33334)
+resultado = conn.multiplicacion(5, 4)
+print(resultado)
+resultado = conn.suma(2, 3)
+print(resultado)
--- server.py
+++ server.py
@@ -1,30 +1,53 @@
 from socket import *
+import json
 
 class Server(object):
     sock = None
     exit = True
+    buffer_receptor = 1024
 
     def __init__(self, address, port):
         self.sock = socket(AF_INET, SOCK_STREAM)
-        self.sock.bind((address, port)) 
+        self.sock.bind((address, port))
         self.sock.listen(1)
-    
+
     def serve(self):
         while exit:
             conn, _ = self.sock.accept()
-            ret = conn.recv(1024).decode()
-            x = "0"
-            if (ret == "1"):
-                x = "2"
-            else:
-                print(ret)
-            
-            conn.send(x.encode())
+
+            # estraccion de data
+            data_devuelta = conn.recv(self.buffer_receptor).decode()
+            data_procesada = json.loads(data_devuelta)
+            args = data_procesada["params"]
+            method = data_procesada["method"]
+
+            # procesamiento de resultado
+            result = getattr(self, method)(*args)
+
+            # envio de resultado
+            data_to_send = {
+                    "jsonrpc": data_procesada["jsonrpc"],
+                    "result": result,
+                    "id": data_procesada["id"]
+                }
+            conn.send(json.dumps(data_to_send).encode())
             conn.close()
         self.sock.close()
 
-    def shutdown():
-        exit = False
+    def add_method(self, function):
+        setattr(self, function.__name__, function)
 
-serv = Server("127.0.0.1", 8080)
+    def shutdown():
+        self.exit = False
+
+
+def suma(a, b):
+    return a + b
+
+def multiplicacion(a, b):
+    return a * b
+
+serv = Server("127.0.0.1", 33334)
+serv.add_method(suma)
+serv.add_method(multiplicacion)
 serv.serve()
