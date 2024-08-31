2cce2320f050afebf91774f12848f90d75180ddd
marcabral <marianaluzcabral@gmail.com>
2024-08-30 21:44:21 -0300

    A lot

--- client.py
+++ client.py
@@ -7,66 +7,49 @@ def generador_de_ids():
     return random.randint(0, 10000000000000)
 
 
-# Clase auxlilar para realizar el llamado al servidor
-class Client_call_ret(object):
-    client_father = None
-    method = None
-
-    def __init__(self, client, method: str):
-        self.client_father = client
-        self.method = method
-
-    def __call__(self, *args):
-        if self.client_father is None:
-            raise Exception("Cliente no encontrado.")
-        elif self.method == "":
-            raise Exception("No hay nomber para el metodo.")
-        else:
-            sock = socket(AF_INET, SOCK_STREAM)
-            sock.connect((self.client_father.address, self.client_father.port))
-            data_a_enviar = {
-                    "jsonrpc": self.client_father.jsonrpc_version,
-                    "method": self.method,
-                    "params": list(args),
-                    "id": generador_de_ids()
-                }
-            data_encoded = json.dumps(data_a_enviar).encode()
-            sock.send(data_encoded)
-
-            data_recivida = sock.recv(
-                self.client_father.buffer_receptor
-            )
-            data_procesada = json.loads(data_recivida.decode())
-            # data_procesada = data_procesada["result"]
-            sock.close()
-
-            return data_procesada
-
-
 class Client(object):
     jsonrpc_version = "2.0"
     buffer_receptor = 1024
-    address = 0
-    port = 0
+    address = None
+    port = None
 
     def __init__(self, address: str, port: int):
         self.address = address
         self.port = port
 
-    def __getattr__(self, method: str) -> Client_call_ret:
-        return Client_call_ret(self, method)
+    def __getattr__(self, method: str) -> callable:
+        def ret(*args):
+            if method == "":
+                raise AttributeError("No hay nombre para el metodo.")
+            elif self.address is None:
+                raise TypeError("Direccion no encontrada.")
+            elif self.address is None:
+                raise TypeError("Puerto no encontrada.")
+            else:
+                sock = socket(AF_INET, SOCK_STREAM)
+                sock.connect((self.address, self.port))
+                data_a_enviar = {
+                        "jsonrpc": self.jsonrpc_version,
+                        "method": method,
+                        "params": list(args),
+                        "id": generador_de_ids()
+                    }
+                data_encoded = json.dumps(data_a_enviar).encode()
+                sock.sendall(data_encoded)
+
+                data_recibida = sock.recv(self.buffer_receptor)
+                data_procesada = json.loads(data_recibida.decode())
+                # data_procesada = data_procesada["result"]
+                sock.close()
+
+                return data_procesada
+        return ret
 
 
 def connect(address: str, port: int) -> Client:
     return Client(address, port)
 
 
-conn = connect("127.0.0.1", 33342)
+conn = connect("127.0.0.1", 33352)
 resultado = conn.multiplicacion(5, 4)
-print(resultado)
-resultado = conn.suma(2, 3)
-print(resultado)
-resultado = conn.division(3, 2)
-print(resultado)
-resultado = conn.suma([], "a")
 print(resultado)
--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 from socket import *
 import json
-
+import threading
 
 class Server(object):
     sock = None
     exit = True
+    queue_length = 5
     buffer_receptor = 1024
 
     def __init__(self, address, port):
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.sock.bind((address, port))
-        self.sock.listen(1)
+        self.sock.listen(self.queue_length)
 
     def get_json_rpc_right(self, jsonrpc, result, id=None):
         if id is not None:
@@ -36,17 +37,21 @@ class Server(object):
             message = "Invalid params"
         elif code == -32603:
             message = "Internal error"
-        elif code < -32099 and code > -32000:
-            message = "Server error"
         return {"jsonrpc": jsonrpc, "error": {"code": code, "message": message}, "id": "null"}
 
     def validate_json_rpc(self, json):
-        # evaluar si esta el campo jsonrpc con un string que representa una version
-        # evaluar si esta el campo method con un string que representa el nombre
-        # evaluar si esta el campo params con una lista con los parametros
-        # evaluar que puede existir id
-        # evaluar que no exista otro campo
-        return True
+        if type(json) == dict:
+            if "jsonrpc" in json and json["jsonrpc"] == "2.0" and "method" in json and type(json["method"]) is str:
+                args = list(json.keys())
+                args.remove("jsonrpc")
+                args.remove("method")
+                if len(args) == 2:
+                    return "id" in args and ("params" in args and type(json["params"]) is list)
+                if len(args) == 1:
+                    return "id" in args or ("params" in args and type(json["params"]) is list)
+                if len(args) == 0:
+                    return True
+        return False
 
     def serve(self):
         while exit:
@@ -55,46 +60,53 @@ class Server(object):
             # recepcion de datos
             data = conn.recv(self.buffer_receptor)
             data = data.decode()
-
+ 
             try:
-                json_rpc = json.loads(data)
-
-                if self.validate_json_rpc(json_rpc):
-                    args = json_rpc["params"]
-                    method = json_rpc["method"]
-
-                    try:
-                        method = getattr(self, method)
+                try:
+                    json_rpc = json.loads(data)
+                except json.decoder.JSONDecodeError:
+                    data_to_send = self.get_json_rpc_error("2.0", -32700)
+                else:
+                    if self.validate_json_rpc(json_rpc):
+                        args = json_rpc["params"]
+                        method = json_rpc["method"]
 
                         try:
-                            result = method(*args)
-                            if json_rpc["id"] is not None:
-                                data_to_send = self.get_json_rpc_right(json_rpc["jsonrpc"], result, json_rpc["id"])
+                            method = getattr(self, method)
+                        except AttributeError:
+                            data_to_send = self.get_json_rpc_error(json_rpc["jsonrpc"], -32601)
+                        else:
+                            try:
+                                result = method(*args)
+                            except TypeError:
+                                data_to_send = self.get_json_rpc_error(json_rpc["jsonrpc"], -32602)
                             else:
-                                data_to_send = self.get_json_rpc_right(json_rpc["jsonrpc"], result)
-                        except Exception:
-                            data_to_send = self.get_json_rpc_error(json_rpc["jsonrpc"], -32602)
-                    except AttributeError:
-                        data_to_send = self.get_json_rpc_error(json_rpc["jsonrpc"], -32601)
-                else:
-                    data_to_send = self.get_json_rpc_error(json_rpc["jsonrpc"], -32600)
-            except json.decoder.JSONDecodeError:
-                data_to_send = self.get_json_rpc_error("2.0", -32700)
+                                if "id" in json_rpc:
+                                    data_to_send = self.get_json_rpc_right(json_rpc["jsonrpc"], result, json_rpc["id"])
+                                else:
+                                    data_to_send = None
+                    else:
+                        data_to_send = self.get_json_rpc_error("2.0", -32600)    
+            except Exception:
+                data_to_send = self.get_json_rpc_error(json_rpc["jsonrpc"], -32603)
 
             # envio de resultado
-            data_to_send = json.dumps(data_to_send)
-            data_to_send = data_to_send.encode()
-            conn.send(data_to_send)
+            if data_to_send is not None:
+                data_to_send = json.dumps(data_to_send).encode()
+                conn.sendall(data_to_send)
+                
             conn.close()
-
+            
     def add_method(self, function):
         setattr(self, function.__name__, function)
 
-    def shutdown():
+    def shutdown(self):
+        self.sock.close()
         self.exit = False
 
 
 def suma(a, b):
+    if type(a) != int or type(b) != int: raise TypeError("")
     return a + b
 
 
@@ -102,7 +114,7 @@ def multiplicacion(a, b):
     return a * b
 
 
-serv = Server("127.0.0.1", 33342)
+serv = Server("127.0.0.1", 33352)
 serv.add_method(suma)
 serv.add_method(multiplicacion)
 serv.serve()
