8bf05da2cea6aaa3a26f470603281aff7b2afa7c
FJBS <Joaquin12201@hotmail.com>
2024-11-17 11:47:57 -0300

    hola

--- lab_2/enrutamiento/sr_pwospf.c
+++ lab_2/enrutamiento/sr_pwospf.c
@@ -416,7 +416,7 @@ void* send_all_lsu(void* arg) {
   return NULL;
 } /* -- send_all_lsu -- */
 
-unsigned construir_packete_lsu (uint8_t ** packet, struct sr_instance* sr, struct sr_if* interface, uint8_t ttl, uint32_t ipDst) {
+unsigned construir_packete_lsu (uint8_t ** packet, struct sr_instance* sr, struct sr_if* interface, uint8_t ttl) {
   pwospf_lock(sr->ospf_subsys);
   unsigned lsas = 0;
   struct sr_rt * elem = sr->routing_table;
@@ -440,7 +440,7 @@ unsigned construir_packete_lsu (uint8_t ** packet, struct sr_instance* sr, struc
   ip_hdr->ip_len = htons(len - sizeof(sr_ethernet_hdr_t));
   ip_hdr->ip_p = htons(ip_protocol_ospfv2);
   ip_hdr->ip_id = 0;
-  ip_hdr->ip_dst = ipDst;
+  ip_hdr->ip_dst = interface->neighbor_ip;
   ip_hdr->ip_src = interface->ip;
   ip_hdr->ip_off = IP_DF;
   ip_hdr->ip_ttl = ttl;
@@ -502,18 +502,22 @@ void* send_lsu(void* arg)
     return NULL;
   }
 
-  /* Construyo el LSU */
   Debug("\n\nPWOSPF: Constructing LSU packet\n");
+  uint32_t ipDst = lsu_param->interface->neighbor_ip;
+
   /* Construcción del paquete. */
   uint8_t * packet;
-  unsigned len = construir_packete_lsu (&packet, lsu_param->sr, lsu_param->interface, 64, lsu_param->interface->neighbor_ip);
+  unsigned len = construir_packete_lsu (&packet, lsu_param->sr, lsu_param->interface, 64);
   sr_ethernet_hdr_t * ether_hdr = (sr_ethernet_hdr_t *)packet;
   
   Debug("\n\nPWOSPF: LSU packet constructed\n");
 
   /* envio del paquete.
    * */
-  struct sr_arpentry * entrada_cache = sr_arpcache_lookup (&(lsu_param->sr->cache), lsu_param->interface->neighbor_ip);
+  struct sr_arpentry * entrada_cache = sr_arpcache_lookup (&(lsu_param->sr->cache), ipDst);
+
+  Debug("\nImprimo la interfaz por la que sale:\n");
+  print_addr_ip_int(ipDst);
 
   /* Se conoce la MAC. 
    * */
@@ -553,7 +557,7 @@ void* send_lsu(void* arg)
 
     struct sr_arpreq * req = sr_arpcache_queuereq (
       &(lsu_param->sr->cache), 
-      lsu_param->interface->neighbor_ip,
+      ipDst,
       packet, 
       len, 
       lsu_param->interface->name
@@ -755,7 +759,7 @@ void* sr_handle_pwospf_lsu_packet(void* arg)
 
       /* Construcción del paquete. */
       uint8_t * packet;
-      unsigned len = construir_packete_lsu (&packet, rx_lsu_param->sr, elem, lsu_hdr->ttl - 1, ipDst);
+      unsigned len = construir_packete_lsu (&packet, rx_lsu_param->sr, elem, lsu_hdr->ttl - 1);
       sr_ethernet_hdr_t * ether_hdr = (sr_ethernet_hdr_t *)packet;
 
       /* envio del paquete.
