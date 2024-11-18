49b6aee910ffac2559321d6b002ee455e0c9b26a
FJBS <Joaquin12201@hotmail.com>
2024-11-17 22:03:45 -0300

    asdg

--- lab_2/enrutamiento/sr_pwospf.c
+++ lab_2/enrutamiento/sr_pwospf.c
@@ -474,15 +474,36 @@ unsigned construir_packete_lsu (uint8_t ** packet, struct sr_instance* sr, struc
 
   ospfv2_lsa_t * lsa_part = (ospfv2_lsa_t *)(*packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t) + sizeof(ospfv2_hdr_t) + sizeof(ospfv2_lsu_hdr_t));
 
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
   struct sr_rt * elem = sr->routing_table;
   while (elem && (elem->admin_dst <= 1)) {
+    Debug("\nEsta Corresponde a la subnet: ");
+    print_addr_ip_int(elem->dest.s_addr);
     lsa_part->subnet = elem->dest.s_addr;
+
+    Debug("\nEsta Corresponde a la mascara: ");
+    print_addr_ip_int(elem->mask.s_addr);
     lsa_part->mask = elem->mask.s_addr;
+
+    Debug("\nEsta es la interfaz: %s", elem->interface);
     lsa_part->rid = sr_get_interface(sr, elem->interface)->neighbor_id;
 
     elem = elem->next;
     lsa_part++;
   }
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
+  Debug("\n------------------------------");
 
   ospf_hdr->csum = ospfv2_cksum(ospf_hdr, ospf_size);
   Debug("->-->>--->> 4\n");
