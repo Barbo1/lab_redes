b4c9c859b7e3b9fde87a8df917cfad428b1409d4
FJBS <Joaquin12201@hotmail.com>
2024-11-18 04:25:20 -0300

    asdg

--- lab_2/enrutamiento/sr_pwospf.c
+++ lab_2/enrutamiento/sr_pwospf.c
@@ -361,14 +361,12 @@ void* send_hello_packet(void* arg) {
 
   /* envio del paquete.
    * */
-  pwospf_lock(hello_param->sr->ospf_subsys);
   sr_send_packet (
     hello_param->sr, 
     packet, 
     len, 
     hello_param->interface->name
   );
-  pwospf_unlock(hello_param->sr->ospf_subsys);
 
   free(packet);
 
@@ -534,14 +532,12 @@ void* send_lsu(void* arg)
 
     /* envio del paquete.
      * */
-    pwospf_lock(lsu_param->sr->ospf_subsys);
     sr_send_packet (
       lsu_param->sr, 
       packet, 
       len, 
       lsu_param->interface->name
     );
-    pwospf_unlock(lsu_param->sr->ospf_subsys);
 
     free(entrada_cache);
 
@@ -728,14 +724,12 @@ void* sr_handle_pwospf_lsu_packet(void* arg)
 
         /* envio del paquete.
          * */
-        pwospf_lock(rx_lsu_param->sr->ospf_subsys);
         sr_send_packet (
           rx_lsu_param->sr, 
           packet, 
           len, 
           elem->name
         );
-        pwospf_unlock(rx_lsu_param->sr->ospf_subsys);
 
         free(entrada_cache);
 
